641cae72406032bbb1dd5f861e11ccf0079c1700
RichardAkkad <richardakkad@hotmail.com>
2025-09-11 20:32:42 +0100

    Update LoginController.java

--- src/main/java/com/richyproject/students/controllers/LoginController.java
+++ src/main/java/com/richyproject/students/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.richyproject.students.controllers;
 
 
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    @GetMapping("/LoginPage")
-    public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {
-        if(error!=null){
-            model.addAttribute("errorMessage" ,"Invalid username or password");
-        }
+    /*@GetMapping("/LoginPage")
+    public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {//required=false means it doesnt need to have "error" as a query parameter name,
+        if(error!=null){//The part error=true is a query parameter — this is what Spring's @RequestParam("error") picks up. whatever is assigned to "name" in a html page
+            model.addAttribute("errorMessage" ,"Invalid username or password");//becomes the query parameter name when a form is submitted (especially via GET
+        }//**whatever is assigned to "name" in the html page is the query parameter name
             return "LoginPage";
 
     }
 
+     */
+
+    @GetMapping("/LoginPage")
+    public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+        if ("no expiry".equals(error)){
+            model.addAttribute("errorMessage","no expiry date for user was found");
+
+        }
+        else if ("expired".equals(error)) {
+            model.addAttribute("errorMessage", "user details have expired");
+
+        } else if ("hours".equals(error)) {
+            model.addAttribute("errorMessage", "login only allowed during buisness hours");
 
 
+        } else if ("username".equals(error)) {
+            model.addAttribute("errorMessage", "invalid username");
+
+        }
+
+     else if ("password".equals(error)) {
+        model.addAttribute("errorMessage", "invalid password");
+
+    }
 
 
-}
+        return "LoginPage";
+
+
+    }
