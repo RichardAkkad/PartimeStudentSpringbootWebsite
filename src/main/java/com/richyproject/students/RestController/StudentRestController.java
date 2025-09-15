44ae342b2788898bffc0db938f3b5339813499ab
RichardAkkad <richardakkad@hotmail.com>
2025-09-15 17:46:58 +0100

    Update StudentRestController.java

--- src/main/java/com/richyproject/students/RestController/StudentRestController.java
+++ src/main/java/com/richyproject/students/RestController/StudentRestController.java
@@ -1,7 +1,25 @@
 package com.richyproject.students.RestController;
 
 public class StudentRestController {
+  @Autowired
+    StudentRepository studentRepository;
 
+    @PostMapping("/SaveAStudent")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student)throws Exception {
+
+        try{
+            Student savedStudent=studentRepository.save(student);
+            return ResponseEntity.ok(savedStudent);
+        }
+          
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+        }
+
+
+
+    }
 
 
 
