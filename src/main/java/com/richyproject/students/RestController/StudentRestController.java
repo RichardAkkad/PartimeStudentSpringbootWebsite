69333e291fdf341d52c7d7adcbea2c7901390147
RichardAkkad <richardakkad@hotmail.com>
2025-09-15 17:47:48 +0100

    Update StudentRestController.java

--- src/main/java/com/richyproject/students/RestController/StudentRestController.java
+++ src/main/java/com/richyproject/students/RestController/StudentRestController.java
@@ -1,5 +1,13 @@
 package com.richyproject.students.RestController;
 
+import com.richyproject.students.model.Student;
+import com.richyproject.students.repository.StudentRepository;
+import com.richyproject.students.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class StudentRestController {
   @Autowired
     StudentRepository studentRepository;
