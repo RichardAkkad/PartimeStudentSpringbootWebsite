67f4e0883113ea45609227a32b2ad7ddaa353f4c
RichardAkkad <richardakkad@hotmail.com>
2025-11-05 12:45:01 +0000

    Refactor saveStudent and deleteStudent methods

--- src/main/java/com/richyproject/students/RestController/StudentRestController.java
+++ src/main/java/com/richyproject/students/RestController/StudentRestController.java
@@ -12,38 +12,65 @@ public class StudentRestController {
   @Autowired
     StudentRepository studentRepository;
 
-   @PostMapping("/SaveAStudent")
-    public ResponseEntity<Student> saveStudent(@RequestBody Student student)throws Exception {
+ 
+    @PostMapping("/SaveAStudent")
+    public ResponseEntity<Void> saveStudent(@RequestBody Student student)throws Exception {
 
         try{
             Student savedStudent=studentRepository.save(student);
-            return ResponseEntity.ok(savedStudent);
+            return ResponseEntity.status(201).build();
 
 
         }
         catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-
+            return ResponseEntity.status(500).build();
+            //If save fails, it's a server/database problem → 500 , not a "not found" problem
         }
 
     }
 
-    @GetMapping("/DeleteStudent")
-    public ResponseEntity<?> deleteStudent(@RequestParam int id){
 
-        try{
-            Optional<Student> deleteStudent=studentRepository.findById(id);
+
+    @GetMapping("/DeleteStudent")
+    public ResponseEntity<?> deleteStudent(@RequestParam int id) {
+
+        try {
+            Optional<Student> deleteStudent = studentRepository.findById(id);
             studentRepository.deleteById(deleteStudent.get().getId());
-            return ResponseEntity.status(200).header("message","student saved").body(deleteStudent.get());
-        }
-        catch(Exception e){
+            return ResponseEntity.status(201).header("message", "student saved").body(deleteStudent.get());
+        } catch (Exception e) {
+            System.out.println(e.fillInStackTrace());
             return ResponseEntity.status(404).body("no student with that id was found");
 
         }
 
 
+    }
 
-     }
+
+         
+
+
+
+    @GetMapping("/searchStudentById")
+    public ResponseEntity<String>  searchStudentViaId(@RequestParam String id) {
+        try {
+            Integer integerId=Integer.parseInt(id);
+            Optional<Student> optionalStudent = studentRepository.findById(integerId);
+            if (optionalStudent.isPresent()) {
+                return ResponseEntity.status(201).body(" the "+optionalStudent+" was saved correctly");
+            }//201 = Created (correct for POST/creating new data)
+            //200 = Success (for GET, PUT, PATCH, DELETE)
+            else{
+                return ResponseEntity.status(404).body("the id "+id+ " was not found");
+            }
+        }
+        catch(NumberFormatException e){
+                return ResponseEntity.badRequest().body("invalid id format, enter digits as id only");//400
+            }
+        catch(Exception e){
+            return ResponseEntity.status(500).body("internal server error occured");
+        }
 
 
 
